c422879a6ede683a952eaf2ddbd5ba2ee077d249
bryandev <brayan7br7@gmail.com>
2025-07-25 09:59:21 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="outline" className="m-4">Click me</Button>
+    </div>
+  );
+}
